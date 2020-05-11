be49c85fda5e6c5912af352b80c168aa8d389ded
H-Pylori <philsaam@gmail.com>
2020-05-11 17:03:50 -0400

    added COVID E-commerce snack

--- src/Home.js
+++ src/Home.js
@@ -7,10 +7,18 @@ import ProductValues from "./modules/views/ProductValues";
 import ProductHowItWorks from "./modules/views/WhatWeDo";
 import ContactCTA from "./modules/views/ContactCTA";
 import CarouselPortfolio from "./modules/views/RecentWork";
-
-
+import CoronaVirus from "./modules/views/CoronaVirus";
 
 function Index() {
+  const [state, setState] = React.useState({
+    open: true,
+  });
+
+  const { open } = state;
+
+  const handleClose = () => {
+    setState({ ...state, open: false });
+  };
   return (
     <React.Fragment>
       <ProductHero />
@@ -18,6 +26,7 @@ function Index() {
       <CarouselPortfolio />
       <ProductHowItWorks />
       <ContactCTA />
+      <CoronaVirus open={open} handleClose={handleClose} />
     </React.Fragment>
   );
 }
---
+++ src/modules/views/CoronaVirus.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { Link } from "react-router-dom";
+//MUI
+import SnackbarContent from "@material-ui/core/SnackbarContent";
+import Snackbar from "@material-ui/core/Snackbar";
+import { withStyles } from "@material-ui/core/styles";
+import {
+  Grid,
+  Typography,
+  Hidden,
+  Button,
+  Divider,
+  IconButton,
+} from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+
+const styles = (theme) => ({
+  Snackbar: { marginBottom: "2S0px" },
+  Content: {
+    backgroundColor: "white",
+    maxWidth: "20%",
+    color: theme.palette.primary.dark,
+  },
+  Header: { fontSize: 22, marginBottom: theme.spacing(3) },
+  Divider: { marginBottom: theme.spacing(3) },
+});
+
+const CoronaVirus = (props) => {
+  const { classes } = props;
+  return (
+    <Hidden xsDown>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        key="1"
+        open={props.open}
+        onClose={props.handleClose}
+        className={classes.Snackbar}
+      >
+        <SnackbarContent
+          className={classes.Content}
+          message={
+            <Grid container>
+              <Grid item>
+                <Typography className={classes.Header}>
+                  Corona Virus and E-commerce
+                </Typography>
+                <Divider className={classes.Divider} />
+                <Typography color="inherit" className={classes.Divider}>
+                  Due to Corona Virus businesses need E-commerce, We can have
+                  you up and selling.
+                </Typography>
+                <Divider />
+              </Grid>
+            </Grid>
+          }
+          action={[
+            <Button
+              variant="contained"
+              color="secondary"
+              component={Link}
+              to="/contact"
+            >
+              get started with Long Leaf
+            </Button>,
+            <IconButton
+              key="close"
+              aria-label="Close"
+              color="inherit"
+              onClick={props.handleClose}
+            >
+              <CloseIcon />
+            </IconButton>,
+          ]}
+        />
+      </Snackbar>
+    </Hidden>
+  );
+};
+
+export default withStyles(styles)(CoronaVirus);
